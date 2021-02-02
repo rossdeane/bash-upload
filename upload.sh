d173381b693eb4d5a375c935dfee91d1f59b67fa
Ross Deane <ross.deane@gmail.com>
2021-02-02 08:21:08 +0000

    add the bash file

---
+++ upload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+S3_BUCKET_NAME=""
+BASE_URL=""
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+filename=$(basename -- "$1")
+extension="${filename##*.}"
+s3cmd put "$1" s3://$S3_BUCKET_NAME/$NEW_UUID.$extension >/dev/null 2>&1
+echo "$BASE_URL/$NEW_UUID.$extension"
